ae60010f29d8d59d9976896833f56126ffa8e70a
Brayan <brayanfiliberto07@gmail.com>
2020-05-02 16:12:37 -0500

    Updates

---
+++ precache-manifest.0e03ff4c23f5217cc3d4871e2b5d9e27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dcf8e57aeaeed83f083f194e52361df",
+    "url": "/App-movies/index.html"
+  },
+  {
+    "revision": "71d89fadb1a4dbf80be4",
+    "url": "/App-movies/static/css/main.d5d113f0.chunk.css"
+  },
+  {
+    "revision": "8c6cfd2c4d1990655793",
+    "url": "/App-movies/static/js/2.4310f17b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/App-movies/static/js/2.4310f17b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d89fadb1a4dbf80be4",
+    "url": "/App-movies/static/js/main.6a71173d.chunk.js"
+  },
+  {
+    "revision": "040f007c1ea9972deb24",
+    "url": "/App-movies/static/js/runtime-main.635564fe.js"
+  }
+]);
